b4cfc6a3e2e1701c4b105b82b6d09aa14e6f3053
wevans <wevans@cix.co.uk>
2013-12-12 15:42:00 +0000

    Modified hello.rb

--- hello.rb
+++ hello.rb
@@ -5,3 +5,27 @@ end
 hello("Rob", 30)
 hello("Jane", 56)
 hello("Pete", 72)
+
+def buy(product, quantity = 1)
+	puts("Here is your #{quantity} #{product}, thank you")
+end
+
+buy("milk")
+
+def monthly_payment(annual_salary)
+	return annual_salary / 12
+end
+
+puts "If the annual salary is £30,000 per year,"
+puts "then the monthly pay is £#{monthly_payment(30000)}"
+
+def odd_or_even(number)
+	if number.odd?
+		"odd"
+	else
+		"even"
+	end
+end
+
+puts ("2 is " + odd_or_even(2))
+puts ("5 is #{odd_or_even(5)}")
