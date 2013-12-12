5318c0a490974bcd3ac3d805bdae6e601b548edc
wevans <wevans@cix.co.uk>
2013-12-12 13:13:48 +0000

    Added makers.rb

---
+++ makers.rb
@@ -0,0 +1,12 @@
+# Assign the string "Makers" to a variable and print it out
+people = "Makers"
+puts "The people variable is currently: " + people
+# Change the variable to "Coders" and print it again
+people = "Coders"
+puts "The people variable is currently: " + people
+# The same program using interpolation of variables
+people = "Makers"
+puts "The people variable with interpolation is currently: #{people} " 
+# Change the variable to "Coders" and print it again
+people = "Coders"
+puts "The people variable with interpolation is currently: #{people} "
